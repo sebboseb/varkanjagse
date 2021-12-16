0a80f2c38be3b4f034b3838859ad6d6944c2d3fa
Sebastian Hallqvist <31276987+sebboseb@users.noreply.github.com>
2021-12-16 21:40:08 +0100

    changes

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -38,7 +38,7 @@ function Mainpage() {
     }
 
     return (
-        <div className="w-screen h-auto md:h-screen flex flex-col bg-slate-800">
+        <div className="w-screen h-screen xl:h-screen flex flex-col bg-slate-800">
             <div className="flex w-screen z-50 absolute justify-end">
                 <div className="flex flex-col">
                     <input className=" w-56 mt-4 mr-8 rounded p-1 px-5 focus:bg-blue-100" type="text" placeholder="Sök" value={query} onChange={onChange} />
@@ -67,21 +67,21 @@ function Mainpage() {
             {/* <h1 className="font-semibold text-3xl text-emerald-400">Murloc</h1> */}
             <div className="w-screen flex flex-col">
                 <div className=" z-10">
-                    <div className=" bg-gradient-to-t dark:via-transparent via-transparent dark:from-letterboxd-bg from-slate-800 md:h-2/3 w-full absolute object-cover z-10"></div>
-                    <div className=" bg-gradient-to-l dark:via-transparent via-transparent dark:from-letterboxd-bg from-slate-800 md:h-2/3 w-full absolute object-cover z-10"></div>
-                    <div className=" bg-gradient-to-r dark:via-transparent via-transparent dark:from-letterboxd-bg from-slate-800 md:h-2/3 w-full absolute object-cover z-10"></div>
-                    <div className=" bg-gradient-to- dark:via-transparent via-transparent dark:from-letterboxd-bg from-slate-800 md:h-2/3 w-full absolute object-cover z-10"></div>
-                    <img className="md:h-2/3 h-96 w-full absolute object-cover md:object-[0px,-100px]" src={`https://image.tmdb.org/t/p/original${moviemovie.backdrop_path}`} alt={moviemovie.name}></img>
+                    <div className=" bg-gradient-to-t dark:via-transparent via-transparent dark:from-letterboxd-bg from-slate-800 xl:h-2/3 h-1/2 w-full absolute object-cover z-10"></div>
+                    <div className=" bg-gradient-to-l dark:via-transparent via-transparent dark:from-letterboxd-bg from-slate-800 xl:h-2/3 h-1/2 w-full absolute object-cover z-10"></div>
+                    <div className=" bg-gradient-to-r dark:via-transparent via-transparent dark:from-letterboxd-bg from-slate-800 xl:h-2/3 h-1/2 w-full absolute object-cover z-10"></div>
+                    <div className=" bg-gradient-to- dark:via-transparent via-transparent dark:from-letterboxd-bg from-slate-800 xl:h-2/3 h-1/2 w-full absolute object-cover z-10"></div>
+                    <img className="xl:h-2/3 h-1/2 w-full absolute object-cover xl:object-[0px,-100px]" src={`https://image.tmdb.org/t/p/original${moviemovie.backdrop_path}`} alt={moviemovie.name}></img>
                 </div>
-                <div className="z-20 w-screen flex flex-col items-center pt-24 md:pt-64">
-                    <img className=" md:w-44 w-32 border border-white rounded shadow-lg shadow-black" src={`https://image.tmdb.org/t/p/original${moviemovie.poster_path}`} alt="" />
-                    <h1 className="md:text-6xl text-4xl font-semibold capitalize mb-8 text-white mt-4">{id.replace(/-/g, " ")}</h1>
+                <div className="z-20 w-screen flex flex-col items-center pt-24 xl:pt-64">
+                    <img className=" xl:w-44 w-32 border border-white rounded shadow-lg shadow-black" src={`https://image.tmdb.org/t/p/original${moviemovie.poster_path}`} alt="" />
+                    <h1 className="xl:text-6xl text-4xl font-semibold capitalize mb-8 text-white mt-4">{id.replace(/-/g, " ")}</h1>
                 </div>
             </div>
-            <div className=" text-white w-screen flex justify-center bg-slate-800">
-                {where.SE && <div className="flex flex-col md:flex-row gap-y-9 md:gap-x-3 justify-evenly w-screen border-t border-slate-600 md:max-w-5xl max-w-xl pt-4 px-3 md:p-0">
+            <div className=" text-white w-screen flex justify-center bg-slate-800 mt-8 xl:mt-0">
+                {where.SE && <div className="flex flex-col xl:flex-row gap-y-9 xl:gap-x-3 justify-evenly w-screen border-t border-slate-600 xl:max-w-5xl max-w-xl pt-4 px-3 xl:p-0">
                     {where.SE.buy &&
-                        <div className="border-t border-slate-600 md:border-0 w-full text-center md:w-auto md:text-left">
+                        <div className="border-t border-slate-600 xl:border-0 w-full text-center xl:w-auto xl:text-left">
                             <u className="font-semibold">Köp</u>
                             <ul className="">
                                 {
@@ -95,7 +95,7 @@ function Mainpage() {
                         </div>
                     }
                     {where.SE.ads &&
-                        <div className="border-t border-slate-600 md:border-0 w-full text-center md:w-auto md:text-left">
+                        <div className="border-t border-slate-600 xl:border-0 w-full text-center xl:w-auto xl:text-left">
                             <u className="font-semibold">Reklam</u>
                             <ul className="">
                                 {where.SE.ads?.map((service) => (
@@ -107,7 +107,7 @@ function Mainpage() {
                         </div>
                     }
                     {where.SE.free &&
-                        <div className="border-t border-slate-600 md:border-0 w-full text-center md:w-auto md:text-left">
+                        <div className="border-t border-slate-600 xl:border-0 w-full text-center xl:w-auto xl:text-left">
                             <u className="font-semibold">Gratis</u>
                             <ul className="">
                                 {where.SE.free?.map((service) => (
@@ -118,7 +118,7 @@ function Mainpage() {
                         </div>
                     }
                     {where.SE.flatrate &&
-                        <div className="border-t border-slate-600 md:border-0 w-full text-center md:w-auto md:text-left">
+                        <div className="border-t border-slate-600 xl:border-0 w-full text-center xl:w-auto xl:text-left">
                             <u className="font-semibold">Streama</u>
                             <ul className="">
                                 {where.SE.flatrate?.map((service) => (
@@ -129,7 +129,7 @@ function Mainpage() {
                         </div>
                     }
                     {where.SE.rent &&
-                        <div className="border-t border-slate-600 md:border-0 w-full text-center md:w-auto md:text-left">
+                        <div className="border-t border-slate-600 xl:border-0 w-full text-center xl:w-auto xl:text-left">
                             <u className="font-semibold">Hyr</u>
                             <ul className="">
                                 {where.SE.rent?.map((service) => (
